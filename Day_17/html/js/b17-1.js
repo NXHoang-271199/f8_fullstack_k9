efc6276a4d39351083167fbf8be61a3c03e1a965
NXHoang-271199 <nguyenxuanhoang271199@gmail.com>
2024-06-23 23:13:12 +0700

    update bai 17

--- Day_17/html/js/b17-1.js
+++ Day_17/html/js/b17-1.js
@@ -1,16 +1,19 @@
 var price = null;
 var km = 121;
-if (km <= 1) {
-    price =  15000;
-} else if (km > 1 && km <= 5) {
-    price = 15000 + (km - 1) * 13500;
-} else if (km > 5) {
-    price = 15000 + 4*13500 + (km - 5)*11000;
-} 
-
-var total = price;
-
-if (km > 120) {
-    total =  total * 0.9;
+if (km > 0) {
+    if (km <= 1) {
+        price =  15000;
+    } else if (km > 1 && km <= 5) {
+        price = 15000 + (km - 1) * 13500;
+    } else if (km > 5) {
+        price = 15000 + 4*13500 + (km - 5)*11000;
+    } 
+    
+    var total = price;
+    
+    if (km > 120) {
+        total =  total * 0.9;
+    }
 }
+
 console.log(total);
--- Day_17/html/js/b17-3.js
+++ Day_17/html/js/b17-3.js
@@ -1,8 +1,8 @@
-n = 5;
+var n = 5;
 var S = 0;
 for (var i = 1; i <= n; i++) { 
     console.log(S)
     S += i * (i + 1);
     
 }
-console.log(S);
+console.log(`S = ${S}`);
